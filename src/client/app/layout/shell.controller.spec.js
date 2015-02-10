950869701fd6c43ee3b9d86dd2fc8b439ef584b9
John Papa <john@johnpapa.net>
2015-02-09 23:29:40 -0500

    removed bard from shell controller spec because we want to show the value of bard.js right after this

--- src/client/app/layout/shell.controller.spec.js
+++ src/client/app/layout/shell.controller.spec.js
@@ -1,22 +1,37 @@
 /* jshint -W117, -W030 */
 describe('layout shell controller', function() {
-
+ 
     var controller;
-
+    var $log;         // different from bard 
+    var $timeout;     // different from bard 
+ 
     beforeEach(function() {
-        bard.appModule('app.layout');
-        bard.inject(this, '$controller', '$log', '$timeout');
+        module('app.layout', fakeToastr);
+ 
+        function fakeToastr($provide) {
+            $provide.constant('toastr', {
+                info: function() {},
+                error: function() {},
+                warning: function() {},
+                success: function() {}
+            });
+        }
+ 
+        // different from bard - begin
+        inject(function($controller, _$log_, _$timeout_) {
+            $log = _$log_;
+            $timeout = _$timeout_;
+            controller = $controller('Shell');
+        });
+        // different from bard - end
+        
     });
-
-    beforeEach(function () {
-        controller = $controller('Shell');
-    });
-
+ 
     it('should have logged success on activation', function() {
         // passes if ANY of the logs matches
         expect($log.info.logs).to.match(/success/i);
     });
-
+ 
     it('should should hide splash after delay', function() {
         var vm = controller;
         expect(vm.showSplash).to.equal(true, 'showSplash before delay');
