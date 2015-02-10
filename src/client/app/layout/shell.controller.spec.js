1b7b0f4062cc0f89da822aed4ac8c1049a502f11
John Papa <john@johnpapa.net>
2015-02-09 23:38:57 -0500

    cleaned whitespace and crap

--- src/client/app/layout/shell.controller.spec.js
+++ src/client/app/layout/shell.controller.spec.js
@@ -1,27 +1,23 @@
 /* jshint -W117, -W030 */
 describe('layout shell controller', function() {
- 
     var controller;
-    var $log;         // different from bard 
-    var $timeout;     // different from bard 
+    var $log;
+    var $timeout;
  
     beforeEach(function() {
         module('app.layout');
  
-        // different from bard - begin
         inject(function($controller, _$log_, _$timeout_, toastr) {
             // Crazy stuff we do to disable the toastr
             toastr.info = function() {};
             toastr.error = function() {};
             toastr.warning = function() {};
             toastr.success = function() {};
-
+ 
             $log = _$log_;
             $timeout = _$timeout_;
             controller = $controller('Shell');
         });
-        // different from bard - end
-        
     });
  
     it('should have logged success on activation', function() {
