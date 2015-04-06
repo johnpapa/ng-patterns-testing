b6b4c05388c4f3696ea1ef423d223abd22556ab9
Ward Bell <wardbell@hotmail.com>
2015-04-06 16:09:57 -0700

    basics.js matching ps-ng-testing module2 script

--- src/basics/basics.js
+++ src/basics/basics.js
@@ -2,73 +2,69 @@
 /* jscs: disable */
 /* Using chai BDD (expect) assertions:
    http://chaijs.com/api/bdd/ */
-describe('basics:', function () {
 
+describe('basics:', function () {
     it('true should be true', function () {
-         expect(true).to.equal(true);
+        expect(true).to.be.true;
     });
 
 
     describe('truthiness', function () {
-
         it('true is truthy', function () {
             expect(true).to.be.ok;
         });
-
         it('1 is truthy', function () {
             expect(1).to.be.ok;
         });
         it('"false" is truthy', function () {
-            expect('false').to.ok;
+            expect('false').to.be.ok;
+        });
+        it('0 is falsey', function () {
+            expect(0).to.be.not.ok;
         });
     });
 
 
-
-
-    describe('#array', function () {
+    describe('array', function () {
         var array;
         beforeEach(function () {
-            array = [1, 2, 3];
+            array = [1,2,3];
         });
 
-
-        it('push adds item to the end', function () {
+        it('push should add item to the end', function () {
             array.push(5);
             expect(array).to.have.length(4);
             expect(array[3]).to.equal(5);
         });
 
-        it('pop removes the last one', function () {
-
+        it('pop should remove the last one', function () {
             var popped = array.pop();
             expect(popped).to.equal(3);
             expect(array).to.have.length(2);
         });
-
-
     });
 
 
 
-
+    // SUT: System Under Test
     function setSeed(seed) {
+
         if (seed === 0) {
+            // throw new Error('some other error');
             throw new Error('0 is a bad seed');
         }
         return seed;
     }
 
-
     describe('going to seed', function () {
         it('should be ok with 3', function () {
             expect(setSeed(3)).to.be.ok;
         });
+
         it('should throw with 0', function () {
             expect(function () {
                 setSeed(0);
             }).to.throw(/bad seed/);
         });
     });
-
 });
